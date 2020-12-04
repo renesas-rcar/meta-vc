b03efd84d9379045f1ec3634b9b7b0c105c1dcf9
Christian Mardmöller <christian.mardmoeller@renesas.com>
2020-12-04 15:38:59 +0100

    Updated ptp scripts and configuration files

--- recipes-rswitch/ptp4lrswitch/files/ptp4l.sh
+++ recipes-rswitch/ptp4lrswitch/files/ptp4l.sh
@@ -8,45 +8,51 @@ PTP4L_CONFIG_FILE=/etc/ptp4l/ptp4l_slave.cfg
 # Load user config
 DEFAULT_CONF_FILE=/etc/ptp4l/ptp4l.conf
 if [ -f "$DEFAULT_CONF_FILE" ]; then
-    . "$DEFAULT_CONF_FILE"
+	. "$DEFAULT_CONF_FILE"
 fi
 
 #sanity check
 if [ "$PTP4L_INTERFACES" == "" ]; then
-    echo "No ptp4l interfaces configured
-    exit 0
+	echo "No ptp4l interfaces configured. ptp4l service not started."
+	exit 0
 fi
 
 if [ ! -f "$PTP4L_CONFIG_FILE" ]; then
-    echo "No ptp4l \"$PTP4L_CONFIG_FILE\" configuration file found
-    exit 1
+	echo "No ptp4l \"$PTP4L_CONFIG_FILE\" configuration file found"
+	exit 1
 fi
 
 
 
 case "$1" in
 start)
-    ## start ptp4l deamon
-    ptp4l -i $PTP4L_INTERFACES -f $PTP4L_CONFIG_FILE -m &   
+	interfaces=
+	for i in $PTP4L_INTERFACES; do
+		interfaces+="-i$i "
+	done
 
-    ## switch status led (LED7) to yellow (started)
-    echo "0" > /sys/class/leds/led7\:green/brightness
-    echo "255" > /sys/class/leds/led7\:orange/brightness
-    ;;
+	## start ptp4l application
+	echo "Start 'ptp4l $interfaces -f $PTP4L_CONFIG_FILE -m'"
+	ptp4l $interfaces -f $PTP4L_CONFIG_FILE -m &
+
+	## switch status led (LED7) to yellow (started)
+	echo "0" > /sys/class/leds/led7\:green/brightness
+	echo "255" > /sys/class/leds/led7\:orange/brightness
+	;;
 
 stop)
-    ## stop ptp4l deamon
-    killall ptp4l
+	## stop ptp4l deamon
+	killall ptp4l
 
-    ## switch status led (LED7) to off
-    echo "0" > /sys/class/leds/led7\:green/brightness
-    echo "0" > /sys/class/leds/led7\:orange/brightness
-    ;;
+	## switch status led (LED7) to off
+	echo "0" > /sys/class/leds/led7\:green/brightness
+	echo "0" > /sys/class/leds/led7\:orange/brightness
+	;;
 
 force-reload|restart)
-    $0 stop
-    $0 start
-    ;;
+	$0 stop
+	$0 start
+	;;
 
 *)
     echo "Usage: $0 {start|stop}"
