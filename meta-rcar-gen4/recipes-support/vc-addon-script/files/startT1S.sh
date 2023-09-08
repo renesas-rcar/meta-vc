78408c4f08da1ca01873fe8457552fe3eb8fdf04
Dennis Ostermann <dennis.ostermann@renesas.com>
2023-09-08 11:59:08 +0200

    [vc4][vc-addon-scripts]: Update T1s files
    According to SDK-3.13 release

--- meta-rcar-gen4/recipes-support/vc-addon-script/files/startT1S.sh
+++ meta-rcar-gen4/recipes-support/vc-addon-script/files/startT1S.sh
@@ -2,52 +2,49 @@
 
 VC_NUM=`dd if=/sys/bus/nvmem/devices/board-data/nvmem bs=1 count=2 skip=1812 status=none | hexdump -e '"%d"'`
 
-#T1S_OVERLAY=~/examples/t1s0.dtbo
+#T1S_OVERLAYS=~/examples/t1s0.dtbo
 
-
-lsmod | grep lan865x_mod
-if [ $? == "0" ]; then
+if [ -d /sys/module/lan865x_mod ]; then
   echo "lan865x driver will be removed"
   rmmod lan865x_mod
-  #remove a potential overlay
-  if [ -d /sys/kernel/config/device-tree/overlays/t1s ]; then
-    rmdir /sys/kernel/config/device-tree/overlays/t1s
+fi
+
+#apply overlaays
+for dtbo in $T1S_OVERLAYS; do
+  ovl=$(basename "$dtbo" .dtbo)
+  ovld=/sys/kernel/config/device-tree/overlays/$ovl
+  if [ -d $ovld ]; then
+    echo "removing existing $ovl overlay"
+    rmdir $ovld
   fi
-fi
-
-
-#apply overlay
-if [ -f "${T1S_OVERLAY}" ]; then
-  mkdir /sys/kernel/config/device-tree/overlays/t1s
-  cat ~/examples/t1s0.dtbo > /sys/kernel/config/device-tree/overlays/t1s/dtbo
-fi
-
-#temporary fix to configure the input filter for low level detection 
-#eth0, intp35
-devmem 0xDFED_4b0c 4
-#eth1, intp34
-devmem 0xDFED_4b08 4
-#eth2, intp33
-devmem 0xDFED_4b04 4
-
+  echo "loading $dtbo"
+  mkdir $ovld
+  cat $dtbo > $ovld/dtbo
+done
 
 #load the driver
 if [ -f /home/root/lan865x_mod.ko ]; then
+  echo "loading custom module from /home/root/lan865x_mod.ko"
   insmod /home/root/lan865x_mod.ko || exit 1
 else
+  echo "loading system lan865x_mod.ko"
   modprobe lan865x_mod || exit 1
 fi
 echo "lan865x driver loaded"
 sleep 1
 
+# Detect eth device names
+spidevs=$(cd /sys/bus/spi/drivers/LAN865x && echo spi*)
+ethdevs=$(for d in $spidevs; do ls /sys/bus/spi/devices/$d/net; done)
 
-#disable IPv6 (the ethX numbers may need update)
-sysctl -w net.ipv6.conf.eth0.disable_ipv6=1
-sysctl -w net.ipv6.conf.eth1.disable_ipv6=1
-sysctl -w net.ipv6.conf.eth2.disable_ipv6=1
+# Disable IPv6
+for eth in $ethdevs; do
+  sysctl -w net.ipv6.conf.$eth.disable_ipv6=1
+done
 
-
-#Add some IP addresses
-ifconfig eth0 6.6.0.$VC_NUM/16
-ifconfig eth1 6.6.1.$VC_NUM/16
-ifconfig eth2 6.6.2.$VC_NUM/16
+# Add some ip addresses
+n=0
+for eth in $ethdevs; do
+  ip addr add 6.6.$n.$VC_NUM/16 dev $eth
+  n=$[$n+1]
+done
